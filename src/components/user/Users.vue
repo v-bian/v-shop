5409462bc325bc214e651fca97091cbc45d5f6e1
边浩东 <1596124143@qq.com>
2021-05-21 22:16:55 +0800

    完成了权限功能的开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -334,7 +334,7 @@ export default {
     },
     // 监听分配角色对话框的关闭事件
     setRoleDialogClosed() {
-      this.selectedRoleId = '', 
+      this.selectedRoleId = ''
       this.userInfo = {}
     }
   }
